729e3b86e440d7084a72cc2c77c8c50c7a5fded8
made1993 <mariogr_93@hotmail.com>
2016-07-25 13:44:29 +0200

    codigos server cliente y conexion

--- src/cliente.c
+++ src/cliente.c
@@ -9,40 +9,68 @@
 #include <unistd.h>
 #include <time.h>
 #include <sys/types.h>
+#include <pthread.h>
 #include "../include/conexion.h"
  
 
-int main(int argc , char *argv[]){
-    char* msg= malloc(8096);
-    int sockfd;
-    struct addrinfo hints, *res;
-    memset(&hints, 0, sizeof hints);
-    hints.ai_family = AF_UNSPEC;
-    hints.ai_socktype = SOCK_STREAM;
+int sockfd = -1;
+pthread_t h1, h2;
+void* hiloEscritura(void* args){
+	char* msg= malloc(8096);
+	if(sockfd ==-1)
+		return NULL;
+	while (1){
 
-    /*Comenzamos la conexion TCP*/
-    printf("se obtiene informacion\n");
-    if(0!=getaddrinfo("192.168.1.42", "8080", &hints, &res)){
-        return 0;
-    }
-    printf("socket\n");
-    sockfd=abrirSocketTCP();
-    if(sockfd==-1){
-        return 0;
-    }
-    printf("connect\n");
-    if(-1==abrirConnect(sockfd, *(res->ai_addr))){
-        
-        return 0;
-    }
+	scanf("%s", msg);
+	if(strcmp("/disconnect",msg) == 0)
+		break;
+	escribir(sockfd, msg);
 
-    scanf("%s", msg);
-    escribir(sockfd, msg);
-
-    return 0;
-
-
-    
+	}
+	free(msg);
+	return NULL;
 }
 
-    
+void* hiloLectura(void* args){
+	char* msg= malloc(8096);
+	if(sockfd ==-1)
+		return NULL;
+	while(1){
+		recibir(sockfd, msg);
+		printf("%s\n", msg);
+	}
+	return NULL;
+}
+
+int main(int argc , char *argv[]){
+	char* msg= malloc(8096);
+	struct addrinfo hints, *res;
+	memset(&hints, 0, sizeof hints);
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_STREAM;
+
+	/*Comenzamos la conexion TCP*/
+	printf("se obtiene informacion\n");
+	if(0!=getaddrinfo("192.168.1.42", "8080", &hints, &res)){
+		return 0;
+	}
+	printf("socket\n");
+	sockfd=abrirSocketTCP();
+	if(sockfd==-1){
+		return 0;
+	}
+	printf("connect\n");
+	if(-1==abrirConnect(sockfd, *(res->ai_addr))){
+		  
+		return 0;
+	}
+
+	pthread_create(&h1,NULL, hiloLectura, (void *)NULL );
+
+	hiloEscritura(NULL);
+	
+	return 0;
+ 
+}
+
+	 
--- src/server.c
+++ src/server.c
@@ -3,13 +3,34 @@
 #include <sys/socket.h>
 #include <arpa/inet.h> //inet_addr
 #include <unistd.h>	//write
+#include <pthread.h>
 #include "../include/conexion.h"
  
+pthread_t* hilos;
+
+void* lecturaUsuario(void* args){
+	int* socket = NULL;
+	char * buff = malloc(8096);
+	socket = (int*) args;
+	while(1){
+
+		if(recibir(*socket, buff) == -1)
+			break;
+		if(strlen(buff) == 0)
+			break;
+		printf("[%s]\n", buff);
+	}
+	printf("Usuario desconectado\n");
+	free(socket);
+	free(buff);
+	return NULL;
+}
 
 int main(){
 	int socket;
-	int socketcli;
-	char * buff = malloc(8096);
+	int* socketcli;
+	int usuarios = 0;
+	
 	struct sockaddr_in ip4addr;
 
 	printf("abrirSocketTCP\n");
@@ -22,9 +43,15 @@ int main(){
 	abrirListen(socket);
 
 
+	while(1){
+		socketcli= malloc(sizeof(int));
+		*socketcli=aceptar(socket, ip4addr);
+		usuarios++;
+		hilos= realloc(hilos, sizeof(pthread_t)*usuarios);
 
-	socketcli=aceptar(socket, ip4addr);
-	recibir(socketcli, buff);
-	printf("%s\n", buff);
+		pthread_create(&hilos[usuarios-1],NULL, lecturaUsuario, (void*) socketcli );
+		
+	}
+	return 0;
 }
 
